7bcca88cbad1f898bbf4547d2aaf9eb85592f0d6
Kanthi Subramanian <subkanthi@gmail.com>
2018-06-21 22:21:59 -0400

    Fixed babel command to compile es6 files

--- src/services/cluster.js
+++ src/services/cluster.js
@@ -2,35 +2,47 @@
 
 var cluster = require('cluster');
 
+/**
+ * Service to create cluster
+ */
 
-class cluster {
+class ClusterService {
 
-    function startCluster() {
-if(cluster.isMaster) {
-    var numWorkers = require('os').cpus().length;
+    constructor() {
 
-    console.log('Master cluster setting up ' + numWorkers + ' workers...');
-
-    for(var i = 0; i < numWorkers; i++) {
-        cluster.fork();
     }
 
-    cluster.on('online', function(worker) {
-        console.log('Worker ' + worker.process.pid + ' is online');
-    });
+    /**
+     * Function to start cluster
+     * @param {*} params 
+     */
+    startCluster(params) {
+  
+        if(cluster.isMaster) {
+            var numWorkers = require('os').cpus().length;
 
-    cluster.on('exit', function(worker, code, signal) {
-        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-        console.log('Starting a new worker');
-        cluster.fork();
-    });
-} else {
-    var app = require('express')();
-    app.all('/*', function(req, res) {res.send('process ' + process.pid + ' says hello!').end();})
+        console.log('Master cluster setting up ' + numWorkers + ' workers...');
 
-    var server = app.listen(8000, function() {
-        console.log('Process ' + process.pid + ' is listening to all incoming requests');
-    });
-}
+        for(var i = 0; i < numWorkers; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('online', function(worker) {
+            console.log('Worker ' + worker.process.pid + ' is online');
+        });
+
+        cluster.on('exit', function(worker, code, signal) {
+            console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+            console.log('Starting a new worker');
+            cluster.fork();
+        });
+    } else {
+            var app = require('express')();
+            app.all('/*', function(req, res) {res.send('process ' + process.pid + ' says hello!').end();})
+
+            var server = app.listen(8000, function() {
+                console.log('Process ' + process.pid + ' is listening to all incoming requests');
+            });
+        }
     }
 }
